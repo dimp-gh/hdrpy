06b7254d6dc24ff550e2566abbe3c5a16b066e6c
Dmitri Pribysh <dmand@yandex.ru>
2018-02-08 16:51:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# hdrpy
+
+`hdrpy` is a pure[^1] Python implementation of HDR histograms. Compatible with Python 2.7 and Python 3.5+.
+
+[^1]: Well, if you not count numpy.
+
+Technically, it can be considered a port of [hdrhistogram](https://github.com/HdrHistogram/HdrHistogram_py/).
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name="hdrpy",
+        version="0.1",
+        description="HDR histogram implementation based on numpy",
+        author="Dmitri Pribysh",
+        autor_email="dmand@yandex.ru",
+        url="https://github.com/dmand/hdrpy",
+        license='Apache 2.0',
+        platform='any',
+
+        packages=["hdrpy"],
+        install_requires=['numpy>=1.14.0'],
+        keywords='hdrpy hdr histogram high dynamic range',
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: System Administrators",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: MacOS",
+            "Operating System :: Windows",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+        ],
+    )
+
