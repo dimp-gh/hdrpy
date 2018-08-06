771427b744e656b831c303bd1d7f258d5b0c688c
Dmitri Pribysh <dmand@yandex.ru>
2018-08-06 18:06:34 +0300

    Prepare the release of 0.3.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="hdrpy",
-        version="0.3.2",
+        version="0.3.3",
         description="HDR histogram implementation based on numpy",
         author="Dmitri Pribysh",
         author_email="dmand@yandex.ru",
